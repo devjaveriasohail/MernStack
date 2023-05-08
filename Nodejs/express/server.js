66f22ee7b6bfa54f378b57084d831d053cd15aaa
devjaveriasohail <javeriasohail996@gmail.com>
2023-05-08 23:38:48 +0500

    week5

--- Nodejs/express/server.js
+++ Nodejs/express/server.js
@@ -1,11 +1,31 @@
 const express = require("express")
-
+const app = express();
 const path = require("path");
 
 //port to host
 const PORT = process.env.PORT || 3500;
 
-server.listen(PORT, () => console.log(`server running on port ${PORT}`))
+app.get("^/$|/index(.html)?",(req,res)=>{
+    //res.sendFile("./views/index.html",{root:__dirname})
+    res.sendFile(path.join(__dirname,"views","index.html"))
+})
+app.get("/new-page(.html)?",(req,res)=>{
+    res.sendFile(path.join(__dirname,"views","new-page.html"))
+})
+app.get("/old-page(.html)?",(req,res)=>{
+    res.redirect(301,"/new-page.html")
+})
+app.get("/*",(req,res)=>{
+    res.status(404).sendFile(path.join(__dirname,"views","404.html"))
+})
+//Route.handelers
+app.get("/hello(.html)?",(req,res,next)=>{
+    console.log("attempted to load hello.html")
+    next()
+},(req,res)=>{
+    res.send("hello world")
+})
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
 
 
 
