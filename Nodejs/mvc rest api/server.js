fa05504eaaf35fa262cb7f52f04ed3ba976d81ed
devjaveriasohail <javeriasohail996@gmail.com>
2023-05-13 12:25:11 +0500

    mvc rest api

--- Nodejs/mvc rest api/server.js
+++ Nodejs/mvc rest api/server.js
@@ -2,32 +2,18 @@ const express = require("express")
 const app = express();
 const path = require("path");
 const cors = require("cors");
+const corsOptions = require ("./config/corsOptions")
 const {logger}= require("./middleware/logEvents")
 const errorHandler= require("./middleware/errorHandler")
 const PORT = process.env.PORT || 3500;
-// custom middleware logger
-// app.use((req,res,next)=>{
-//     logEvents(`${req.method}\t ${req.headers.origin}\t ${req.url}`,"reqLog")
-//     console.log(`${req.method} ${req.path}`)
-// next();
-// })
-app.use(logger);
-// Cross origin Resource Sharing
-const whitelist =["https://www.yoursite.com","http://127.0.0.1:5500","http://localhost:3500"]
-const corsOptions={
-    origin:(origin,callback)=>{
-        if (whitelist.indexOf(origin)!== -1|| !origin){
-            callback(null,true)
 
-        }else {
-            callback(new Error ("not allowed by CORS"))
-        }
-    }, optionsSuccessStatus:200
-}
+// custom middleware logger
+app.use(logger);
+
+// cross origin Resource Sharing
 app.use(cors(corsOptions))
 
-// middleware any thing between resquest and response(req, res) => {res.redirect(301, "/new-page.html")
-//there are 3 types of middleware 1.builtin middleware 2.custom middleware 3.middleware form 3rd parties
+
 // built-in middleware to handle urlencoded data in other words, form data: "content-type : application/x-www-form-urlencoded"
 app.use(express.urlencoded({extend:false}))
 
@@ -36,11 +22,10 @@ app.use(express.json());
 
 //serve static files 
 app.use("/",express.static(path.join(__dirname, 'public')));
-app.use("/subdir",express.static(path.join(__dirname, 'public')));
 
 // routes
 app.use("/",require("./routes/root"));
-app.use("/subdir",require("./routes/subdir"))
+
 app.use("/employees",require("./routes/api/employees"))
 
 
