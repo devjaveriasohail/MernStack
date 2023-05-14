7968a0ccc4d3a237eb8ebf6cbb2741d3ed313cf4
devjaveriasohail <javeriasohail996@gmail.com>
2023-05-14 21:59:14 +0500

    week 5&6

---
+++ Nodejs/authentication/controllers/authController.js
@@ -0,0 +1,27 @@
+const usersDB = {
+    users: require("../model/users.json"),
+    setUsers: function(data){
+        this.users= data 
+    }
+}
+
+const bcrypt = require("bcrypt");
+const handleLogin = async (req,res)=>{
+    const {user,pwd}= req.body;
+    if (!user || !pwd )return res.status(400).json({"message": "username and password are required"})
+    const foundUser = usersDB.users.find(person=>person.username === user);
+    if (!foundUser) return res.sendStatus(401);
+    //Unauthorized
+    //evalute password
+    const match = await bcrypt.compare(pwd,foundUser.password)
+
+        if(match){
+            // create Jwts
+            res.json({"success":`user ${user} is logged in!`})
+        } else {
+
+            res.sendStatus(401);
+        }
+    }
+
+module.exports ={handleLogin}
