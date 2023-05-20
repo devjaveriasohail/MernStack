9d3d1f06697cf82ddc619849381fcf0145f07c4f
devjaveriasohail <javeriasohail996@gmail.com>
2023-05-20 16:58:36 +0500

    week 7

---
+++ Mern/backend/controlles/userController.js
@@ -0,0 +1,80 @@
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+const asyncHandler = require("express-async-handler")
+const User = require("../model/userModel")
+
+// @desc Register new User  @routes POST/api/users
+const registerUser = asyncHandler(async(req,res)=>{
+    const {name,email,password}= req.body
+    if(!name || !email || !password){
+        res.status(400)
+        throw new Error ("Please add all fields")
+    }
+    // check if user exist
+    const userExits = await User.findOne ({email})
+    if(userExits){
+        res.status(400)
+        throw new Error ("user already exists")
+    }
+    // hash password
+const salt = await bcrypt.genSalt(10)
+const hashedPassword = await bcrypt.hash(password,salt)
+
+// create user
+const user = await User.create({
+    name,email,password:hashedPassword
+})
+if(user){
+    res.status(201).json({
+        _id:user.id,
+        name: user.name,
+        email: user.email,
+        token:generateToken(user._id),
+    })
+}
+else{
+    res.status(400)
+    throw new Error ("Invalid user data")
+}
+    // res.json({
+    //     message:"Register User"
+    // })
+})
+
+// @desc Authenticate User  @routes POST/api/users/login
+const loginUser = asyncHandler(async(req,res)=>{
+    const {email,password} = req.body
+
+    // check for user email
+    const user = await User.findOne({email})
+    if (user&& (await bcrypt.compare(password, user.password))){
+        res.json ({
+            _id:user.id,
+            name: user.name,
+            email: user.email,
+            token:generateToken(user._id)
+        })
+    }else{
+        res.status(400)
+        throw new Error ("Invalid credentials")
+    }
+
+})
+
+// @desc Get User data @routes GET/api/users/me
+const getMe =asyncHandler(async (req,res)=>{
+    res.json({
+        message:"User data display"
+    })
+})
+
+// Generate JWT
+const generateToken=(id)=>{
+    return jwt.sign({id},process.env.JWT_SECRET,{ expiresIn:"90d"})
+}
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe
+}
